6a86df64d49e0d7dfe4f1773abe920a612f41b86
pauldedward <edward2000ed@gmail.com>
2021-07-14 11:03:55 +0530

    caesar refactor

--- Day 8/caesar/caesarCipher.py
+++ Day 8/caesar/caesarCipher.py
@@ -1,12 +1,10 @@
+import os
 
-direction = input("Type 'e' for encryption or 'd' for decryption: ")
 
-if direction != 'e' and direction != 'd':
-    print("Error: Invalid direction")
-    exit()
+def clear(): return os.system('cls')
 
-message = input("Enter the message: ")
-caesarNumber = int(input("Enter the number of shifts: "))
+
+should_continue = True
 
 
 def cipherUnrestricted(message, direction, caesarNumber):
@@ -17,8 +15,12 @@ def cipherUnrestricted(message, direction, caesarNumber):
                 cipheredMessage += chr((ord(letter) -
                                         97 + caesarNumber) % 26 + 97)
             else:
-                cipheredMessage += chr((ord(letter) -
-                                        48 + caesarNumber) % 10 + 48)
+                characterNumber = ord(letter) - 48
+                if characterNumber < 0 or characterNumber > 9:
+                    cipheredMessage += letter
+                else:
+                    cipheredMessage += chr((characterNumber +
+                                            caesarNumber) % 10 + 48)
         return cipheredMessage
     elif direction == 'd':
         for letter in message:
@@ -26,9 +28,29 @@ def cipherUnrestricted(message, direction, caesarNumber):
                 cipheredMessage += chr((ord(letter) -
                                         97 - caesarNumber) % 26 + 97)
             else:
-                cipheredMessage += chr((ord(letter) -
-                                        48 - caesarNumber) % 10 + 48)
+                characterNumber = ord(letter) - 48
+                if characterNumber < 0 or characterNumber > 9:
+                    cipheredMessage += letter
+                else:
+                    cipheredMessage += chr((characterNumber -
+                                            caesarNumber) % 10 + 48)
         return cipheredMessage
 
 
-print(cipherUnrestricted(message, direction, caesarNumber))
+while should_continue:
+    direction = input("Type 'e' for encryption or 'd' for decryption: ")
+
+    if direction != 'e' and direction != 'd':
+        print("Error: Invalid direction")
+        continue
+
+    clear()
+    mode = "Encryption mode" if direction == 'e' else "Decryption mode"
+    print(mode)
+    message = input("Enter the message: ")
+    caesarNumber = int(input("Enter the number of shifts: "))
+
+    print(cipherUnrestricted(message, direction, caesarNumber))
+
+    should_continue = input("Continue? (y/n): ") == 'y'
+
