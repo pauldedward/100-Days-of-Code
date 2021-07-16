1cf9bf839ef339b2c051fbb5f29119afb49092a6
pauldedward <edward2000ed@gmail.com>
2021-07-16 10:31:27 +0530

    day 14 high low

---
+++ Day 14/higer or lower/main.py
@@ -0,0 +1,74 @@
+
+from gameData import data
+from art import logo, vs
+import random
+import os
+clear = lambda: os.system('cls')
+
+def displayGameplay(personA, personB, score, isRight):
+    clear()
+    print(logo)
+    
+    if isRight:
+        print(f"You are right Yourscore is {score}")
+    else:
+        print(f"You are wrong Your final score is {score}")
+        return
+    print(f'Compare A: {personA["name"]}, {personA["description"]}, from {personA["country"]}')
+    print(vs)
+    print(f'With : {personB["name"]}, {personB["description"]}, from {personB["country"]}')
+
+def getPerson():
+    person = random.choice(data)
+    data.remove(person)
+    return person
+
+def initializeGame():
+    if len(data) >= 2:
+        personA = getPerson()
+        personB = getPerson()
+        
+        clear()
+        print(logo)
+        print(f'Compare A: {personA["name"]}, {personA["description"]}, from {personA["country"]}')
+        print(vs)
+        print(f'With : {personB["name"]}, {personB["description"]}, from {personB["country"]}')
+        
+        return personA, personB
+    else:
+        return None,None
+
+def play():
+    
+    personA, personB = initializeGame()
+    
+    if personA == None or personB == None:
+        print("Data curopt")
+        return
+    
+    isRight = True
+    score = 0
+    while isRight:
+        user_choice = input("Who has more followers? choose (A/B) : ").lower()
+        
+        if personA["follower_count"] > personB["follower_count"]:
+            answer = "a"
+        elif personA["follower_count"] < personB["follower_count"]:
+            answer = "b"
+        else:
+            answer = user_choice
+        
+        if user_choice == answer:
+            score += 1
+            
+        else:
+            isRight = False
+            
+        personA = personB
+        personB = getPerson()
+        displayGameplay(personA, personB, score, isRight)
+
+
+
+while input("Want to play higher or lower ? (y/n) ") == "y":
+    play()
