43f10351e347529df0794e20e296cb35424948a9
pauldedward <edward2000ed@gmail.com>
2021-07-18 14:10:29 +0530

    hirst 1

---
+++ Day-18/Hirst/hirst.py
@@ -0,0 +1,14 @@
+# import colorgram
+# colors = colorgram.extract('Day-18\Hirst\dot.jpg',10)
+
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     c = (r,g,b)
+#     color_pallette.append(c)
+# print(color_pallette)
+
+color_pallette = [(234, 166, 59), (45, 112, 157), (113, 150, 203), (212, 123, 164), (16, 128, 96), (172, 44, 88), (1, 177, 143)]
+
+
