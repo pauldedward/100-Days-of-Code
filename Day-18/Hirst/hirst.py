bda06b96d9495284d1579bbe648fc1167810292c
pauldedward <edward2000ed@gmail.com>
2021-07-18 14:32:08 +0530

    hirst initial

--- Day-18/Hirst/hirst.py
+++ Day-18/Hirst/hirst.py
@@ -9,6 +9,32 @@
 #     color_pallette.append(c)
 # print(color_pallette)
 
+import random
+import turtle
+
 color_pallette = [(234, 166, 59), (45, 112, 157), (113, 150, 203), (212, 123, 164), (16, 128, 96), (172, 44, 88), (1, 177, 143)]
 
+turtle.colormode(255)
+my_turtle = turtle.Turtle()
+my_screen = my_turtle.getscreen()
 
+my_turtle.speed(10)
+my_turtle.penup()
+my_turtle.setpos(-250,-250)
+step_size = 50
+my_turtle.pensize(10)
+
+for row in range(1,11):
+    for column in range(1,11):
+        my_turtle.speed(3)
+        my_turtle.pencolor(random.choice(color_pallette))
+        my_turtle.pendown()
+        my_turtle.dot()
+        my_turtle.penup()
+        if column < 10:
+            my_turtle.forward(step_size)
+    
+    my_turtle.speed(10)
+    my_turtle.setpos(my_turtle.xcor() - step_size * 9, my_turtle.ycor() + step_size)
+
+my_screen.exitonclick()
