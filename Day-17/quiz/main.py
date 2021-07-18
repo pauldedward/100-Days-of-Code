b92694bc51948e1f1e79b8f7cc8ceef7acf77bfd
pauldedward <edward2000ed@gmail.com>
2021-07-18 11:17:22 +0530

    quiz ending

--- Day-17/quiz/main.py
+++ Day-17/quiz/main.py
@@ -1,5 +1,5 @@
 from question_model import Question
-from data import question_data
+from data import q_bank,question_data
 from quiz_brain import QuizBrain
 from art import logo
 import os
@@ -10,9 +10,12 @@ clear = lambda: os.system('cls')
 
 question_bank = []
 
-for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+# for question in question_data:
+#     question_bank.append(Question(question["text"], question["answer"]))
 
+for q in q_bank:
+    question_bank.append(Question(q["question"], q["correct_answer"]))
+    
 random.shuffle(question_bank)
 
 quiz_brain = QuizBrain(question_bank)
