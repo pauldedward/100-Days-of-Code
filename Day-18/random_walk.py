6f5fe3d7ce65a5c36c555a56fdf2da6fc7bc1055
pauldedward <edward2000ed@gmail.com>
2021-07-18 12:35:41 +0530

    random walk

---
+++ Day-18/random_walk.py
@@ -0,0 +1,25 @@
+import random
+from turtle import Turtle
+
+my_turtle = Turtle()
+my_screen = my_turtle.getscreen()
+my_turtle.speed(1)
+colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "grey", "brown"]
+
+step_size = 20
+
+my_turtle.pensize(6)
+
+while True:
+    choice = random.randint(0, 3)
+    my_turtle.color(random.choice(colors))
+    
+    if choice == 0:
+        my_turtle.setpos(my_turtle.xcor() + step_size, my_turtle.ycor())
+    elif choice == 1:
+        my_turtle.setpos(my_turtle.xcor() - step_size, my_turtle.ycor())
+    elif choice == 2:
+        my_turtle.setpos(my_turtle.xcor(), my_turtle.ycor() + step_size)
+    elif choice == 3:
+        my_turtle.setpos(my_turtle.xcor(), my_turtle.ycor() - step_size)
+
