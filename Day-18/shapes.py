b4e1015dd93c1aa07a0cb6ba05520feb4a0d5531
pauldedward <edward2000ed@gmail.com>
2021-07-18 12:20:47 +0530

    shapes turtle

---
+++ Day-18/shapes.py
@@ -0,0 +1,21 @@
+import random
+from turtle import Turtle
+
+my_turtle = Turtle()
+my_screen = my_turtle.getscreen()
+colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "grey", "brown"]
+
+my_turtle.penup()
+my_turtle.goto(-75,75)
+my_turtle.pendown()
+
+for sides in range(4,11):
+    my_turtle.color(random.choice(colors))
+    for turns in range(sides):
+        angle = 360 / sides
+        my_turtle.forward(100)
+        my_turtle.right(angle)
+
+
+my_screen.exitonclick()
+
