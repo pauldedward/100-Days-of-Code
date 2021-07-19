ba66b668ca573d0aea0ebdf004b5932791822c8c
pauldedward <edward2000ed@gmail.com>
2021-07-19 15:30:11 +0530

    t race end

--- Day-19/turtle_race.py
+++ Day-19/turtle_race.py
@@ -1,27 +1,49 @@
 from turtle import Turtle, Screen
 import random
 
-colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "grey", "brown"]
+colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink"]
+random.shuffle(colors)
 
 screen = Screen()
 screen.setup(width=500, height=400)
 
-user_bet = screen.textinput(title = "Make your Bet", prompt = "Enter turtle color :").lower()
-
 todds = []
+
+
+while True:
+    user_bet = screen.textinput(title = "Make your Bet", prompt = "Enter turtle color :").lower()
+    
+    if user_bet in colors:
+        is_race_on = True
+        break
+    else:
+        print("Not valid colour, choose one from (red , green, blue, yellow, orange, purple, pink) ")
+
 
 for n in range(0, 7):
     y_pos = -150 + n * 50
     todd = Turtle(shape = "turtle")
-    todd.color(random.choice(colors))
+    todd.color(colors[n])
     todd.penup()
     todd.goto(-240, y_pos)
-    todd.pendown()
     todds.append(todd)
 
 
-
-
+while is_race_on:
+    
+    for todd in todds:
+        
+        if todd.xcor() > 230:
+            
+            todd_color = todd.pencolor()
+            if user_bet == todd_color:
+                print(f"You have won!!! {todd_color} turtle won the race")
+            else:
+                print(f"You lost!!! {todd_color} turtle won the race")
+            is_race_on = False
+            
+        step_size = random.randint(0,10)
+        todd.forward(step_size)
 
 
 screen.exitonclick()
