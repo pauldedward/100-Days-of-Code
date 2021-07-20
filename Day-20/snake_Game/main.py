ccb55e49cf39f67f61576e7868061c25bfb924aa
pauldedward <edward2000ed@gmail.com>
2021-07-20 08:44:06 +0530

    day 20 complete

--- Day-20/snake_Game/main.py
+++ Day-20/snake_Game/main.py
@@ -9,45 +9,45 @@ screen.bgcolor("black")
 screen.title("SnakeD")
 
 snake = Snake()
-screen.listen()
 direction = 0
+
 
 def move_right():
     global direction
     direction = 0
-    
+
+
 def move_left():
     global direction
     direction = 180
 
+
 def move_up():
     global direction
     direction = 90
-    
+
+
 def move_down():
     global direction
     direction = 270
 
 
+screen.listen()
 screen.onkey(move_up, "Up")
 screen.onkey(move_down, "Down")
 screen.onkey(move_left, "Left")
 screen.onkey(move_right, "Right")
 # def reset():
 #     tur.reset()
-    
+
 
 # screen.onkey(reset, "r")
 
 while True:
     screen.update()
     time.sleep(.125)
-    
+
     snake.move(direction)
-    
-    
-
-
 
 
 screen.exitonclick()
--- Day-20/snake_Game/snake.py
+++ Day-20/snake_Game/snake.py
@@ -1,9 +1,12 @@
 from turtle import Turtle
 
+
 class Snake:
+
     def __init__(self):
+
         self.body = []
-        for i in range (0,3):
+        for i in range(0, 3):
             todd = Turtle()
             todd.penup()
             todd.color("white")
@@ -11,17 +14,17 @@ class Snake:
             todd.setx(20 - (i * 20))
             self.body.append(todd)
         self.head = self.body[0]
-    
+
     def move(self, heading):
-    
+
         head_heading = self.head.heading()
-        if heading !=  head_heading and abs(heading - head_heading) != 180:
+        if heading != head_heading and abs(heading - head_heading) != 180:
             self.head.setheading(heading)
 
         prev_x = self.head.xcor()
         prev_y = self.head.ycor()
         self.head.forward(20)
-        
+
         for i in range(1, len(self.body)):
             current_x = self.body[i].xcor()
             current_y = self.body[i].ycor()
