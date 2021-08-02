dc0e20ed7c0a101021176044b4e268ebd802fde4
pauldedward <edward2000ed@gmail.com>
2021-08-02 16:29:29 +0530

    common_numbers

--- Day-26/common_numbers/main.py
+++ Day-26/common_numbers/main.py
@@ -4,6 +4,7 @@ with open("./Day-26/common_numbers/file_1.txt") as file:
 with open("./Day-26/common_numbers/file_2.txt") as file:
     array_two = file.readlines()
 
+# result = [int(num) for num in array_one if num in array_two]
 array_one = [int(x.strip()) for x in array_one]
 array_two = [int(x.strip()) for x in array_two]
 
@@ -11,5 +12,5 @@ result = []
 for x in array_one:
     if x in array_two:
         result.append(x)
-        
+
 print(result)
