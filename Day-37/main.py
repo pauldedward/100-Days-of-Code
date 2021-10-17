78c1a8f35c2d745db0d0ace7a8a488128c5162a3
pauldedward <edward2000ed@gmail.com>
2021-10-17 15:33:47 +0530

    pixela

---
+++ Day-37/main.py
@@ -0,0 +1,41 @@
+import requests
+import datetime as dt
+
+pixela_endpoint = 'https://pixe.la/v1/users'
+username = 'pauldedward'
+token = 'ilgsm.0o'
+
+user_params = {
+    'token': token,
+    'username': username,
+    'agreeTermsOfService':'yes',
+    'notMinor':'yes',
+}
+# response = requests.post(url=pixela_endpoint, json=user_params)
+graph_endpoint = 'https://pixe.la/v1/users/{}/graphs'.format(username)
+
+graph_config = {
+    'id': 'graph1',
+    'name':'100-days-of-code',
+    'unit':'minutes',
+    'type':'int',
+    'color':'sora'
+}
+
+headers = {
+    'X-USER-TOKEN': token,
+}
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+# $ curl -X POST https://pixe.la/v1/users/a-know/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20180915","quantity":"5"}'
+# {"message":"Success.","isSuccess":true}
+pixel_endpoint = 'https://pixe.la/v1/users/{}/graphs/{}'.format(username, graph_config['id'])
+
+pixel_config = {
+    'date': dt.datetime.now().strftime('%Y%m%d'),
+    'quantity': '120',
+}
+
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+
+# print(response.text)
